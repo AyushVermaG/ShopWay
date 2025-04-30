81b291512c82402e2cceeba283265fd442710d4d
Ayush Verma <ayush0067.becse24@chitkara.edu.in>
2025-04-30 21:24:10 +0530

    Implemented full-page auto-sliding hero section with some addition to html,css & js

--- ShopWay.js
+++ ShopWay.js
@@ -114,3 +114,14 @@ function showSection(section) {
 
     content.innerHTML = html;
 }
+
+  const slides = document.querySelectorAll('.hero-slide');
+  let current = 0;
+
+  setInterval(() => {
+    slides[current].classList.remove('active');
+
+    current = (current + 1) % slides.length;
+
+    slides[current].classList.add('active');
+  }, 4000);
